cf45950d7730a60c459b60e679b280afb95ecb37
MCrowley91 <142123698+MCrowley91@users.noreply.github.com>
2023-08-25 17:36:38 -0700

    Homework19
    Test to delete an existing playlist

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test (description = "Homework19")
+    public void deletePlaylist() throws InterruptedException {
+
+        String deletionConfirmation = "Deleted playlist \"Playlist to be deleted.\"";
+
+        navigateToPage();
+        provideEmailAddress("mary.crowley@testpro.io");
+        providePassword("Password");
+        clickSubmit();
+        Thread.sleep(2000);
+        WebElement playlistToDelete = driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(5) > a"));
+        playlistToDelete.click();
+        Thread.sleep(2000);
+        WebElement deletePlaylistButton = driver.findElement(By.cssSelector("[title='Delete this playlist']"));
+        deletePlaylistButton.click();
+        Assert.assertEquals(getSuccessMessage(), deletionConfirmation);
+    }
+
+    public String getSuccessMessage() {
+        WebElement notificationMessage = driver.findElement(By.cssSelector(".success.show"));
+        return notificationMessage.getText();
+    }
+}
