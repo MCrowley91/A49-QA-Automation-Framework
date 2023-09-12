a17c3c694bba91d0e64f25aae95293b00abf2327
MCrowley91 <142123698+MCrowley91@users.noreply.github.com>
2023-09-12 15:18:35 -0700

    Homework27
    Installed Cucumber for TestNG and configured it for login test case

---
+++ src/test/java/Homework27.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.RegistrationPage;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class Homework27 extends BaseTest {
+    LoginPage loginPage;
+    HomePage homePage;
+    RegistrationPage registrationPage;
+
+    @BeforeMethod
+    public void testSetup(){
+        loginPage = new LoginPage(getThreadDriver());
+        homePage = new HomePage(getThreadDriver());
+    }
+
+    @Test (priority = 1, description = "Homework 27")
+    public void loginTest(){
+        loginPage = new LoginPage(getThreadDriver());
+        homePage = new HomePage(getThreadDriver());
+        loginPage.provideEmail("mary.crowley@testpro.io")
+                .providePassword("Password")
+                .clickSubmit();
+        Assert.assertTrue(homePage.getUserAvatar().isDisplayed());
+    }
+
+    @Test (priority = 2, description = "Another Homework 27 Test")
+    public void registrationButtonTest(){
+        registrationPage = new RegistrationPage(getThreadDriver());
+        registrationPage.clickRegistrationButton();
+        wait.until(ExpectedConditions.urlMatches("https://qa.koel.app/registration"));
+        //Assert.assertEquals(getThreadDriver().getCurrentUrl(), "https://qa.koel.app/registration"));
+    }
+}
